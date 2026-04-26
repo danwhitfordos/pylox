723c879dcb94dd85b2a504907e297d897f3b3848
Dan Whitford <dan@whitford.io>
2026-04-26 14:01:59 +0100

    parse very simple

--- parser.py
+++ parser.py
@@ -2,28 +2,25 @@ from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Any
 
-from lexer import Lexer
+from lexer import Lexer, TokenType
 
-class StatementType(Enum):
-    RETURN = auto()
+class NodeType(Enum):
     INT_ATOM = auto()
 
 @dataclass
-class Statement():
-    type: StatementType
+class Node():
+    type: NodeType
     value: Any = None
+
 
 class Parser():
     def __init__(self, lexer: Lexer):
         self.lexer = lexer
 
-    def get_atom(self):
+    def get_next_node(self):
         t = self.lexer.get_next_token()
+        if t.type == TokenType.EOF:
+            return None
+        elif t.type == TokenType.INTEGER:
+            return Node(NodeType.INT_ATOM, t.value)
 
-    def get_expression(self):
-        self.get_atom()
-        self.consume_expected(TokenType.SEMICOLON)
-
-    def get_next_statement(self):
-        self.get_expression()
-        return Statement(StatementType.RETURN)
--- test_parser.py
+++ test_parser.py
@@ -1,25 +1,24 @@
 import unittest
 
-from parser import Statement, StatementType
+from parser import Node, NodeType
 
 class TestParser(unittest.TestCase):
     
-    def test_parser_eof(self):
+    def test_parser_empty(self):
         from parser import Parser
         from lexer import Lexer
 
         parser = Parser(Lexer(""))
-        want = Statement(StatementType.RETURN)
-        got = parser.get_next_statement()
-        self.assertEqual(got, want)
+        got = parser.get_next_node()
+        self.assertEqual(got, None)
 
     def test_parser_onenumber(self):
         from parser import Parser
         from lexer import Lexer
 
         parser = Parser(Lexer("1;"))
-        want = Statement(StatementType.INT_ATOM, 1)
-        got = parser.get_next_statement()
+        want = Node(NodeType.INT_ATOM, 1)
+        got = parser.get_next_node()
         self.assertEqual(got, want)
 
 if __name__ == '__main__':
